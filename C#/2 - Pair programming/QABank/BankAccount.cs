76910a03bc7a42938debd436a16c26ab04daa116
pveysey1992@googlemail.com <pveysey1992@googlemail.com>
2022-11-14 13:49:58 +0000

    Added Bank Account class

---
+++ C#/2 - Pair programming/QABank/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QABank
+{
+    public class BankAccount
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CustomerName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int AccountNumber { get; set; }
+
+        public int Balance 
+
+    }
+}
